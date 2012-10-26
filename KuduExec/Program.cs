98eb75523a5bd0c7e694493c6f048333be3dbebf
davidebbo <david.ebbo@microsoft.com>
2012-10-25 23:27:59 -0700

    Execute initial command to get folder + cleanup

--- KuduExec/Program.cs
+++ KuduExec/Program.cs
@@ -12,95 +12,98 @@ namespace KuduExec
         {
             if (args.Length < 1)
             {
-                Console.WriteLine("Usage: {0} [kudu service url]", typeof(Program).Assembly.GetName().Name);
+                Console.WriteLine("Usage: {0} [kudu service url (with username)]", typeof(Program).Assembly.GetName().Name);
             }
-            else
+            try
             {
-                string uriString = args[0];
-                var uri = new Uri(uriString);
-                string userName = uri.UserInfo;
-
-                if (!uriString.EndsWith("/"))
-                {
-                    uriString = uriString + "/";
-                }
-
-                uriString = uriString + "command";
-
-                var handler = new HttpClientHandler();
-                if (!String.IsNullOrEmpty(userName))
-                {
-                    Console.Write("Enter password (WARNING: will echo!): ");
-                    // TODO: don't echo password! :)
-                    string password = Console.ReadLine();
-                    handler.Credentials = new NetworkCredential(userName, password);
-                }
-
-                HttpClient client = new HttpClient(handler);
-                string command = null;
-                string currentFolder = "";
-                ShowPrompt(currentFolder);
-
-                while ((command = Console.ReadLine()) != null)
-                {
-                    command = command.Trim();
-
-                    try
-                    {
-                        // Ignore empty lines
-                        if (String.IsNullOrEmpty(command))
-                        {
-                            continue;
-                        }
-
-                        // Add a 'cd' at the end so we can get the working folder on the way out
-                        command = command + " & cd";
-
-                        JObject payload = new JObject(new JProperty("command", command), new JProperty("dir", currentFolder));
-                        JObject result = client.PostAsJsonAsync<JObject>(uriString, payload).Result.Content.ReadAsAsync<JObject>().Result;
-                        string output = result.Value<string>("Output");
-                        string error = result.Value<string>("Error");
-                        int exitCode = result.Value<int>("ExitCode");
-
-                        if (!string.IsNullOrEmpty(error))
-                        {
-                            Console.WriteLine(error);
-                            continue;
-                        }
-
-                        output = output.TrimEnd();
-
-                        // The last like should be the working folder so parse it out
-                        int lastLineIndex = output.LastIndexOf("\r\n");
-
-                        if (lastLineIndex < 0)
-                        {
-                            currentFolder = output;
-                        }
-                        else
-                        {
-                            currentFolder = output.Substring(lastLineIndex);
-
-                            Console.WriteLine(output.Substring(0, lastLineIndex));
-                        }
-
-                        currentFolder = currentFolder.Trim();
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine(exception.Message);
-                    }
-                    finally
-                    {
-                        ShowPrompt(currentFolder);
-                    }
-                }
+                Run(args[0]);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
             }
         }
 
-        private static void ShowPrompt(string currentFolder)
+        private static void Run(string kuduServiceUriString)
         {
-            Console.Write("[Kudu] {0}> ", currentFolder);
+            string userName = (new Uri(kuduServiceUriString)).UserInfo;
+
+            if (!kuduServiceUriString.EndsWith("/"))
+            {
+                kuduServiceUriString = kuduServiceUriString + "/";
+            }
+
+            kuduServiceUriString = kuduServiceUriString + "command";
+
+            var handler = new HttpClientHandler();
+            if (!String.IsNullOrEmpty(userName))
+            {
+                Console.Write("Enter password (WARNING: will echo!): ");
+                // TODO: don't echo password! :)
+                string password = Console.ReadLine();
+                handler.Credentials = new NetworkCredential(userName, password);
+            }
+
+            HttpClient client = new HttpClient(handler);
+            string currentFolder = "";
+
+            bool first = true;
+            string command = "cd";
+
+            for (; ; )
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Console.Write("[Kudu] {0}> ", currentFolder);
+                    command = Console.ReadLine();
+                    if (command == null) break;
+
+                    command = command.Trim();
+
+                    // Ignore empty lines
+                    if (String.IsNullOrEmpty(command)) continue;
+
+                    // Add a 'cd' command at the end so we can get the working folder on the way out
+                    command = command + " & cd";
+                }
+
+                JObject payload = new JObject(new JProperty("command", command), new JProperty("dir", currentFolder));
+                HttpResponseMessage responseMessage = client.PostAsJsonAsync<JObject>(kuduServiceUriString, payload).Result.EnsureSuccessStatusCode();
+
+                JObject result = responseMessage.Content.ReadAsAsync<JObject>().Result;
+                string output = result.Value<string>("Output");
+                string error = result.Value<string>("Error");
+                int exitCode = result.Value<int>("ExitCode");
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Console.WriteLine(error);
+                    continue;
+                }
+
+                output = output.TrimEnd();
+
+                // The last line should be the working folder (from the 'cd' command) so parse it out
+                int lastLineIndex = output.LastIndexOf("\r\n");
+
+                if (lastLineIndex < 0)
+                {
+                    currentFolder = output;
+                }
+                else
+                {
+                    currentFolder = output.Substring(lastLineIndex);
+
+                    Console.WriteLine(output.Substring(0, lastLineIndex));
+                    Console.WriteLine();
+                }
+
+                currentFolder = currentFolder.Trim();
+            }
         }
     }
 }
